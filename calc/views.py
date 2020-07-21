0305c000103363831e0118a939af2d2388aa4832
Girisha Devara <girisha.devara@gmail.com>
2020-07-21 11:57:39 +0530

    calculator

---
+++ calc/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def calc(request):
+    if request.method == 'POST':
+        value1 = int(request.POST.get('value1'))
+        value2 = int(request.POST.get('value2'))
+        operation = request.POST.get('operation')
+    else:
+        value1 = int(request.GET.get('value1'))
+        value2 = int(request.GET.get('value2'))
+        operation = request.GET.get('operation')
+    if operation == ' ':
+        ans = value1 + value2
+    elif operation == '-':
+        ans = value1 - value2
+    elif operation == '*':
+        ans = value1 * value2
+    elif operation == '/':
+        ans = value1 / value2
+    return JsonResponse({'Output': ans})
