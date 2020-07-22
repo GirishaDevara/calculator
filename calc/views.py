07cfd61e835e88ab5bde32593a642c6d8c78ccc1
Girisha Devara <girisha.devara@gmail.com>
2020-07-22 09:22:35 +0530

    csrf token error rectified

--- calc/views.py
+++ calc/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def calc(request):
     if request.method == 'POST':
         value1 = int(request.POST.get('value1'))
